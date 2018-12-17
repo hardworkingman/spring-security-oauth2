6b1e903c8121032b90edd55f0bfda27ca12f4a10
songxincong <1205780548@qq.com>
2018-12-17 17:08:36 +0800

    Squashed commit of the following:
    commit a5001b4f3ae4588680c79dcb2beffcde22644c6d
    Author: songxincong <1205780548@qq.com>
    Date:   Mon Dec 17 17:08:14 2018 +0800
    
        多登录方式集成

--- src/main/java/com/oauth2/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/oauth2/demo/service/UserDetailsServiceImpl.java
@@ -1,19 +1,61 @@
 package com.oauth2.demo.service;
 
+import com.oauth2.demo.integration.IntegrationAuthentication;
+import com.oauth2.demo.integration.IntegrationAuthenticationContext;
+import com.oauth2.demo.integration.authenticator.IntegrationAuthenticator;
 import com.oauth2.demo.user.entity.AuthUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service("userDetailsService")
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private List<IntegrationAuthenticator> authenticators;
+
+    @Autowired(required = false)
+    public void setAuthenticators(List<IntegrationAuthenticator> authenticators) {
+        this.authenticators = authenticators;
+    }
+
     @Override
     public AuthUser loadUserByUsername(String username) throws UsernameNotFoundException {
-        AuthUser user = new AuthUser();
-        user.setUsername("admin");
-        user.setPassword("{bcrypt}$2a$10$cKRbR9IJktfmKmf/wShyo.5.J8IxO/7YVn8twuWFtvPgruAF8gtKq");
-        user.setEnabled(true);
-        return user;
+        IntegrationAuthentication integrationAuthentication = IntegrationAuthenticationContext.get();
+        // 判断是否是集成登录
+        if (integrationAuthentication == null) {
+            integrationAuthentication = new IntegrationAuthentication();
+        }
+        integrationAuthentication.setUsername(username);
+        AuthUser authUser = authenticate(integrationAuthentication);
+
+        if (authUser == null) {
+            throw new UsernameNotFoundException("用户名或密码错误");
+        }
+        setAuthorize(authUser);
+        authUser.setEnabled(true);
+        return authUser;
+    }
+
+    /**
+     * 设置授权信息
+     *
+     * @param user
+     */
+    private void setAuthorize(AuthUser user) {
+        // TODO 设置角色权限信息
+    }
+
+    private AuthUser authenticate(IntegrationAuthentication integrationAuthentication) {
+        if (this.authenticators != null) {
+            for (IntegrationAuthenticator authenticator :authenticators) {
+                if (authenticator.support(integrationAuthentication)) {
+                    return authenticator.authenticate(integrationAuthentication);
+                }
+            }
+        }
+        return null;
     }
 }
